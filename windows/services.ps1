dfb7b327a07c7ff722f40d77294889762bdb525c
Nicolas Le Gall <nicolaslegall34@gmail.com>
2015-08-03 21:44:45 +0200

    Add some Windows services

--- windows/services.ps1
+++ windows/services.ps1
@@ -3,7 +3,7 @@ echo "=============================="
 echo "|     Services Windows       |"
 echo "=============================="
 
-$serviceList = "AdobeARMservice","CscService"
+$serviceList = "iphlpsvc","lmhosts","SCardSvr","TrkWks","SessionEnv","UI0Detect","QWAVE","WinRM","p2pimsvc","VaultSvc","fdPHost","WdiSystemHost","SNMPTRAP","RpcLocator","lltdsvc","Netlogon","SharedAccess","wercplsupport","AxInstSV","CertPropSvc","PNRPsvc","FDResPub","UmRdpService","RemoteRegistry","WbioSrvc","BDESVC","WwanSvc","PcaSvc","ALG","bthserv","PNRPAutoReg","WerSvc","DPS","WPDBusEnum","WdiServiceHost","MSiSCSI","WMPNetworkSvc","TermService","SCPolicySvc","WcsPlugInService","Fax","WebClient","wcncsvc"
 
 for($i = 0; $i -le $serviceList.length - 1; $i++)
 {
