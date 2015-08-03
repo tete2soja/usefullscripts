f90feb34a1a4c0f9760a9e87835236e094fc5b5e
Nicolas Le Gall <nicolaslegall34@gmail.com>
2015-08-03 23:07:46 +0200

    some decoration

--- windows/services.ps1
+++ windows/services.ps1
@@ -1,8 +1,15 @@
-# Array for services
+# =============================================================================
+# Perform few actions inorder to optimise Windows :
+#  - desactive useless services
+#  - reduce waiting time for shutdown
+# =============================================================================
+
+
 echo "=============================="
 echo "|     Services Windows       |"
 echo "=============================="
 
+# Array for services
 $serviceList = "iphlpsvc","lmhosts","SCardSvr","TrkWks","SessionEnv","UI0Detect","QWAVE","WinRM","p2pimsvc","VaultSvc","fdPHost","WdiSystemHost","SNMPTRAP","RpcLocator","lltdsvc","Netlogon","SharedAccess","wercplsupport","AxInstSV","CertPropSvc","PNRPsvc","FDResPub","UmRdpService","RemoteRegistry","WbioSrvc","BDESVC","WwanSvc","PcaSvc","ALG","bthserv","PNRPAutoReg","WerSvc","DPS","WPDBusEnum","WdiServiceHost","MSiSCSI","WMPNetworkSvc","TermService","SCPolicySvc","WcsPlugInService","Fax","WebClient","wcncsvc"
 
 for($i = 0; $i -le $serviceList.length - 1; $i++)
