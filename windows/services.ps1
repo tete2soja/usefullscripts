dac00711a201d95288d333af03388cf42850fe2b
Nicolas Le Gall <nicolaslegall34@gmail.com>
2015-08-04 21:00:17 +0200

    Add services for Windows 7
    Service only present for Windows 7

--- windows/services.ps1
+++ windows/services.ps1
@@ -10,6 +10,7 @@ echo "|     Services Windows       |"
 echo "=============================="
 
 # Array for services
+$serviceW7 = "hidserv","napagent","PeerDistSvc","SensrSvc","IPBusEnum","CscService","AppMgmt","hkmsvc","p2psvc","WPCSvc","ehSched","ehRecvr","TabletInputService","TBS","idsvc"
 $serviceList = "iphlpsvc","lmhosts","SCardSvr","TrkWks","SessionEnv","UI0Detect","QWAVE","WinRM","p2pimsvc","VaultSvc","fdPHost","WdiSystemHost","SNMPTRAP","RpcLocator","lltdsvc","Netlogon","SharedAccess","wercplsupport","AxInstSV","CertPropSvc","PNRPsvc","FDResPub","UmRdpService","RemoteRegistry","WbioSrvc","BDESVC","WwanSvc","PcaSvc","ALG","bthserv","PNRPAutoReg","WerSvc","DPS","WPDBusEnum","WdiServiceHost","MSiSCSI","WMPNetworkSvc","TermService","SCPolicySvc","WcsPlugInService","Fax","WebClient","wcncsvc"
 
 for($i = 0; $i -le $serviceList.length - 1; $i++)
