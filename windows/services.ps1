7d2d5d1b24568af86c50c6c3c27625714b8a3ffa
Darkitty <nicolaslegall34@gmail.com>
2015-08-05 11:17:40 +0200

    Add test on Windows version

--- windows/services.ps1
+++ windows/services.ps1
@@ -13,6 +13,17 @@ echo "=============================="
 $serviceW7 = "hidserv","napagent","PeerDistSvc","SensrSvc","IPBusEnum","CscService","AppMgmt","hkmsvc","p2psvc","WPCSvc","ehSched","ehRecvr","TabletInputService","TBS","idsvc"
 $serviceList = "iphlpsvc","lmhosts","SCardSvr","TrkWks","SessionEnv","UI0Detect","QWAVE","WinRM","p2pimsvc","VaultSvc","fdPHost","WdiSystemHost","SNMPTRAP","RpcLocator","lltdsvc","Netlogon","SharedAccess","wercplsupport","AxInstSV","CertPropSvc","PNRPsvc","FDResPub","UmRdpService","RemoteRegistry","WbioSrvc","BDESVC","WwanSvc","PcaSvc","ALG","bthserv","PNRPAutoReg","WerSvc","DPS","WPDBusEnum","WdiServiceHost","MSiSCSI","WMPNetworkSvc","TermService","SCPolicySvc","WcsPlugInService","Fax","WebClient","wcncsvc"
 
+if ((Get-CimInstance Win32_OperatingSystem).Version -like '6.1.*') {
+    echo "Windows 7 dececte"
+    for($i = 0; $i -le $serviceList.length - 1; $i++)
+    {
+        Write-Host "Service"$serviceW7[$i]"desactive" -nonewline
+        echo ""
+        Set-Service -Name $serviceW7[$i] -Computer localhost -StartupType "Disabled"
+    }
+}
+
+
 for($i = 0; $i -le $serviceList.length - 1; $i++)
 {
     Write-Host "Service"$serviceList[$i]"desactive" -nonewline
