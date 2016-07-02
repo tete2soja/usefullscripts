e6195884012822cab713a4b5af05332bb20b03b8
Nicolas Le Gall <contact@nlegall.fr>
2016-07-02 20:07:48 +0200

    Add functions and content
    Création de fonctions pour les graphes et datagridview
    Mise à jour des informations récupérées

--- windows/stats_pc.ps1
+++ windows/stats_pc.ps1
@@ -1,78 +1,148 @@
-$correspondances = @{}
+function createChart {
+    param(
+        $position,
+        $data,
+        [string]$description
+        )
+    # create chart object 
+    $Chart = New-object System.Windows.Forms.DataVisualization.Charting.Chart
+    $Chart.Width = $position[0]
+    $Chart.Height = $position[1]
+    $Chart.Left = $position[2]
+    $Chart.Top = $position[3]
+    # create a chartarea to draw on and add to chart
+    $ChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
+    $Chart.ChartAreas.Add($ChartArea)
+    # add title and axes labels
+    [void]$Chart.Titles.Add($description)
+    # add data to chart
+    [void]$Chart.Series.Add("Data")
+    $Chart.Series["Data"].Points.DataBindXY($data.Keys, $data.Values)
+    # Find point with max/min values and change their colour
+    $maxValuePoint = $Chart.Series["Data"].Points.FindMaxByValue()
+    $maxValuePoint.Color = [System.Drawing.Color]::Red
+    $minValuePoint = $Chart.Series["Data"].Points.FindMinByValue()
+    $minValuePoint.Color = [System.Drawing.Color]::Green
+    # change chart area colour
+    $Chart.BackColor = [System.Drawing.Color]::Transparent
+    $Chart.Series["Data"].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
+    # set chart options
+    $Chart.Series["Data"]["PieLabelStyle"] = "Outside"
+    $Chart.Series["Data"]["PieLineColor"] = "Black"
+    $Chart.Series["Data"]["PieDrawingStyle"] = "Concave"
+    ($Chart.Series["Data"].Points.FindMaxByValue())["Exploded"] = $true
+    # display the chart on a form
+    $Chart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor
+                    [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Leff
+    return $Chart
+}
+
+function createDataGridView {
+    param(
+        $form,
+        $position,
+        $data,
+        [string]$description
+        )
+
+    $dataGridView = New-Object System.Windows.Forms.DataGridView
+    $dataGridView.AutoSize = $true
+    $dataGridView.AutoSizeColumnsMode = "AllCells"
+    $form.Controls.Add($dataGridView)
+    $dataGridView.ColumnCount = 2
+    $dataGridView.top = $position[0]
+    $dataGridView.left = $position[1]
+    $dataGridView.ColumnHeadersVisible = $true
+    $dataGridView.Columns[0].Name = $description
+    $dataGridView.Columns[1].Name = "Nombre"
+    foreach($value in $data.keys) {
+        $dataGridView.Rows.Add($value, $data[$value])
+    }
+}
+
+$OS = @{}
+$RAM = @{}
+$CPU = @{}
+$CORE = @{}
 
 $tri = Import-Csv C:\stats.csv
 
 $unique = $tri | select -Unique buildnumber,name
 
 foreach( $value in $unique) {
-    Write-Host $value
     $count = $($tri | where -filter {$_.buildnumber -eq $value.buildnumber -and $_.name -like "*server*"} | measure).Count
-    if ( $count -ne 0 ) { $correspondances.Add($value.Name, $count) }
+    if ( $count -ne 0 ) { $OS.Add($value.Name, $count) }
     $count = $($tri | where -filter {$_.buildnumber -eq $value.buildnumber -and $_.name -notlike "*server*"} | measure).Count
-    if ( $count -ne 0 ) { $correspondances.Add($value.Name, $count) }
+    if ( $count -ne 0 ) { $OS.Add($value.Name, $count) }
 }
 
-Write-Host $correspondances["Microsoft Windows Server 2012 R2 Standard"]
-Write-Host $correspondances["Widows 8.1"]
+$unique = $tri | select -Unique ram
+
+foreach( $value in $unique) {
+    Write-Host $value
+    $count = $($tri | where -filter {$_.ram -eq $value.ram} | measure).Count
+    if ( $count -ne 0 ) { $RAM.Add($value.ram, $count) }
+}
+
+$unique = $tri | select -Unique cpu
+
+foreach( $value in $unique) {
+    Write-Host $value
+    $count = $($tri | where -filter {$_.cpu -eq $value.cpu} | measure).Count
+    if ( $count -ne 0 ) { $CPU.Add($value.cpu, $count) }
+}
+
+$unique = $tri | select -Unique coeur
+
+foreach( $value in $unique) {
+    Write-Host $value
+    $count = $($tri | where -filter {$_.coeur -eq $value.coeur} | measure).Count
+    if ( $count -ne 0 ) { $CORE.Add($value.coeur, $count) }
+}
 
 # load the appropriate assemblies 
-[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms.DataVisualization")
 
+$position = @(300,225,40,30)
+$Chart = createChart $position $OS "Repartition des OS"
 
+$position = @(300,225,440,30)
+$Chart2 = createChart $position $RAM "Repartition de la RAM"
 
+$position = @(300,225,40,330)
+$Chart3 = createChart $position $CPU "Repartition des modeles CPU"
 
+$position = @(300,225,440,330)
+$Chart4 = createChart $position $CORE "Repartition du nombre de coeurs"
 
-# create chart object 
-$Chart = New-object System.Windows.Forms.DataVisualization.Charting.Chart 
-$Chart.Width = 600 
-$Chart.Height = 450 
-$Chart.Left = 40 
-$Chart.Top = 30
-
-
-
-# create a chartarea to draw on and add to chart 
-$ChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea 
-$Chart.ChartAreas.Add($ChartArea)
-
-# add title and axes labels 
-[void]$Chart.Titles.Add("Repartition des OS") 
-
-
-# add data to chart 
-[void]$Chart.Series.Add("Data") 
-$Chart.Series["Data"].Points.DataBindXY($correspondances.Keys, $correspondances.Values)
-
-# Find point with max/min values and change their colour 
-$maxValuePoint = $Chart.Series["Data"].Points.FindMaxByValue() 
-$maxValuePoint.Color = [System.Drawing.Color]::Red
-
-$minValuePoint = $Chart.Series["Data"].Points.FindMinByValue() 
-$minValuePoint.Color = [System.Drawing.Color]::Green
-
-
-
-# change chart area colour 
-$Chart.BackColor = [System.Drawing.Color]::Transparent
-
-
-$Chart.Series["Data"].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
-# set chart options 
-$Chart.Series["Data"]["PieLabelStyle"] = "Outside" 
-$Chart.Series["Data"]["PieLineColor"] = "Black" 
-$Chart.Series["Data"]["PieDrawingStyle"] = "Concave" 
-($Chart.Series["Data"].Points.FindMaxByValue())["Exploded"] = $true
-
-
-
-# display the chart on a form 
-$Chart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor 
-                [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left 
 $Form = New-Object Windows.Forms.Form 
-$Form.Text = "OS" 
-$Form.Width = 800 
-$Form.Height = 800 
-$Form.controls.add($Chart) 
-$Form.Add_Shown({$Form.Activate()}) 
+$Form.Text = "OS"
+$Form.Width = 800
+$Form.Height = 800
+
+#$pathlabel = New-Object System.Windows.Forms.Label
+#$pathlabel.top = 250
+#$pathlabel.left = 60
+#$pathlabel.Width = 350
+#$pathlabel.Height = 60
+#$Font = New-Object System.Drawing.Font("Times New Roman",12)
+#$pathlabel.Font =  $Font
+#$pathlabel.Text = $text
+#$Form.controls.add($pathlabel)
+
+createDataGridView $form @(250,60) $OS "OS"
+
+createDataGridView $form @(250,560) $RAM "RAM en GB"
+
+createDataGridView $form @(550,60) $CPU "CPU model"
+
+createDataGridView $form @(550,560) $CORE "Nb cores"
+
+
+$Form.controls.add($Chart)
+$Form.controls.add($Chart2)
+$Form.controls.add($Chart3)
+$Form.controls.add($Chart4)
+$Form.Add_Shown({$Form.Activate()})
 $Form.ShowDialog()
