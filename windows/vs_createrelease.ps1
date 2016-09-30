181cde929f2d45dffbf5993d5643bcdd3b312c3f
Nicolas Le Gall <contact@nlegall.fr>
2016-09-30 16:40:44 +0200

    Add MSBuild and fix path

--- windows/vs_createrelease.ps1
+++ windows/vs_createrelease.ps1
@@ -1,7 +1,8 @@
-Copy-Item .\Release\* .\Final\ -Recurse
-rm .\Final\*vshost*
-rm .\Final\*.pdb
-Compress-Archive -Path .\Final\* -DestinationPath release.zip
-Write-Host "SHA256 :"(Get-FileHash -Algorithm SHA256 .\release.zip).Hash
-Write-Host "MD5 :"(Get-FileHash -Algorithm SHA256 .\release.zip).Hash
-Remove-Item -Force -Recurse .\Final
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /nologo /ds /t:build /p:Configuration=Release
+Copy-Item .\bin\Release .\bin\Final -Recurse
+rm .\bin\Final\*vshost*
+rm .\bin\Final\*.pdb
+Compress-Archive -Path .\bin\Final\* -DestinationPath .\bin\release.zip
+Write-Host "SHA256 :"(Get-FileHash -Algorithm SHA256 .\bin\release.zip).Hash
+Write-Host "MD5 :"(Get-FileHash -Algorithm MD5 .\bin\release.zip).Hash
+Remove-Item -Force -Recurse .\bin\Final
